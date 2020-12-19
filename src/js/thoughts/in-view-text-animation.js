4fb7de79ee09a7aa3c2ff248224c9ef7c640bfe5
Brian Anders <anders.brian@icloud.com>
2020-12-19 11:35:50 -0800

    minor refinements to the text-animation timing

--- src/js/thoughts/in-view-text-animation.js
+++ src/js/thoughts/in-view-text-animation.js
@@ -9,7 +9,7 @@ module.exports = {
     const querySelector = 'h1, h2, h3, h4, h5, h6, p';
     Array.from(document.querySelectorAll(querySelector)).forEach((element) => {
       inView(element, inViewOutOfView, {
-        rootMargin: '-70px 0px -70px 0px',
+        rootMargin: '-70px 0px -20px 0px',
       });
     });
   },
