f093fad17d043c235539d1c01c339cccf0851a4f
Brian Anders <anders.brian@icloud.com>
2022-01-14 21:44:19 -0800

    blue/green tester: Polish the logic, add reset button, and change the copy instructions.

--- src/js/thoughts/blue-green.js
+++ src/js/thoughts/blue-green.js
@@ -1,11 +1,14 @@
 (function blueGreen() {
-  const { floor, max, random } = Math;
+  const {
+    floor, min, max, random,
+  } = Math;
   const { log } = console;
   const MAX_COLOR = 255;
 
   const canvas = document.getElementById('canvas');
   const blueButton = document.getElementById('blue-button');
   const greenButton = document.getElementById('green-button');
+  const resetButton = document.getElementById('reset');
   const submitButton = document.getElementById('submit');
   const chartElement = document.getElementById('chart');
   const chartBlue = chartElement.querySelector('.blue');
@@ -14,21 +17,31 @@
   const buttonsElement = document.querySelector('.buttons');
   const revealElement = document.querySelector('.reveal');
 
-  let colorDirection = null;
-  let step = 32;
+  let colorDirection;
+  let step;
 
   const colors = {
     red: 0,
-    green: floor((MAX_COLOR / 2) + (random() * (MAX_COLOR / 2))),
-    blue: floor((MAX_COLOR / 2) + (random() * (MAX_COLOR / 2))),
+    green: 0,
+    blue: 0,
   };
 
+  function reset() {
+    colorDirection = null;
+    step = 32;
+    colors.green = floor((MAX_COLOR / 2) + (random() * (MAX_COLOR / 2)));
+    colors.blue = floor((MAX_COLOR / 2) + (random() * (MAX_COLOR / 2)));
+  }
+
   function updateColors() {
+    colors.blue = max(min(colors.blue, MAX_COLOR), 0);
+    colors.green = max(min(colors.green, MAX_COLOR), 0);
+
     canvas.style.backgroundColor = `rgb(${colors.red}, ${colors.green}, ${colors.blue})`;
     log(canvas.style.backgroundColor);
 
-    let blueDiff;
-    let greenDiff;
+    let blueDiff = 0;
+    let greenDiff = 0;
 
     if (colors.green < colors.blue) {
       blueDiff = colors.blue - colors.green;
@@ -37,6 +50,14 @@
       greenDiff = colors.green - colors.blue;
       blueDiff = 0;
     }
+
+    // log({
+    //   blueDiff,
+    //   greenDiff,
+    //   green: colors.green,
+    //   blue: colors.blue,
+    // });
+
     chartBlue.innerHTML = blueDiff;
     chartBlue.dataset.value = blueDiff;
     chartBlue.style.background = `linear-gradient(to left, blue 0%, blue ${blueDiff / 2}%, transparent ${blueDiff / 2}%)`;
@@ -102,5 +123,13 @@
     revealElement.style.display = null;
   });
 
+  resetButton.addEventListener('click', () => {
+    buttonsElement.style.display = null;
+    revealElement.style.display = 'none';
+    reset();
+    updateColors();
+  });
+
+  reset();
   updateColors();
 }());
