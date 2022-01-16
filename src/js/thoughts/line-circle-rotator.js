d9b9305c68ff2474988cb19a56b8b9ac1094d321
Brian Anders <anders.brian@icloud.com>
2022-01-16 15:32:11 -0800

    polishing the line/circle rotator

--- src/js/thoughts/line-circle-rotator.js
+++ src/js/thoughts/line-circle-rotator.js
@@ -1,3 +1,44 @@
 (function init() {
+  const maxSet = 8;
 
+  function a(power) {
+    const containerElement = document.querySelector('.circle');
+    containerElement.innerHTML = '';
+
+    const setLength = 2 ** power;
+    const setStep = 180 / setLength;
+    const duration = 2000 / setLength;
+
+    for (let set = 0; set < setLength; set++) {
+      const angle = set * setStep;
+      const delay = set * duration;
+      const span = document.createElement('span');
+      const line = document.createElement('line');
+      // span.style.animationDelay = `${delay}ms`;
+      span.style.setProperty('--animation-delay', `${delay}ms`);
+      line.style.setProperty('--animation-delay', `${delay}ms`);
+      line.style.setProperty('transform', `translate(-50%,-50%) rotate(${angle}deg)`);
+      span.style.setProperty('transform', `translate(-50%,-50%) rotate(${angle}deg)`);
+
+      containerElement.appendChild(span);
+      containerElement.appendChild(line);
+    }
+  }
+
+  function setUpSelect() {
+    const select = document.getElementById('select');
+    for (let i = 1; i < maxSet; i++) {
+      const option = document.createElement('option');
+      option.value = i;
+      option.text = 2 ** i;
+      select.options.add(option);
+    }
+
+    select.addEventListener('change', (evt) => {
+      a(Number(evt.target.value));
+    });
+  }
+
+  setUpSelect();
+  a(1);
 }());
