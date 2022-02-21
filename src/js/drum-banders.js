e0568d1fb85325a0e8f5d28e6944913538ea7c43
Brian Anders <anders.brian@icloud.com>
2022-02-21 03:11:41 -0800

    drum banders youtube modal initial commit

--- src/js/drum-banders.js
+++ src/js/drum-banders.js
@@ -1,3 +1,7 @@
-(function init() {
+const YoutubeModal = require('./_modules/youtube-modal');
 
+(function init() {
+  const youtubeModal = new YoutubeModal();
+  console.log('drumBanders.init()');
+  youtubeModal.init();
 }());
