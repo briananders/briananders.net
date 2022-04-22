a0cae8dad8566cb667cdc6ab000d0a90022615f8
Brian Anders <banders@google.com>
2022-04-21 22:00:36 -0700

    working wordle solver

--- src/js/thoughts/wordle-solver/index.js
+++ src/js/thoughts/wordle-solver/index.js
@@ -1,3 +1,5 @@
+const { WorkDocs } = require('aws-sdk');
+
 (function init() {
 
   const dictionary = require('./five-letter-words');
@@ -7,30 +9,134 @@
   const lines = Array.from(boardElement.querySelectorAll('.line'));
   const textInputs = Array.from(boardElement.querySelectorAll('input[type=text'));
   const checkboxes = Array.from(boardElement.querySelectorAll('input[type=radio]'));
-  const closeCheckboxes = checkboxes.filter(box => box.id.includes('close'));
-  const correctCheckboxes = checkboxes.filter(box => box.id.includes('correct'));
-  const wrongCheckboxes = checkboxes.filter(box => box.id.includes('wrong'));
   const submitButtons = Array.from(boardElement.querySelectorAll('button'));
 
   const isLetter = (value) => /[a-zA-Z]/.test(value);
 
-  let goodLetters = [];
-  let badLetters = [];
-  let correctLetters = new Array(5);
-
   function getLetters() {
-    debugger;
+    let closeLetters = [];
+    let wrongLetters = [];
+    let correctLetters = new Array(5);
+    let cannotBeLetters = [[],[],[],[],[]];
+
+    textInputs.forEach(inputElement => {
+      const { state, letterNumber } = inputElement.dataset;
+      const letterIndex = Number(letterNumber);
+      const { value } = inputElement;
+      if (value === '') return;
+
+      if (state === 'wrong') {
+        wrongLetters.push(value);
+      } else if (state === 'close') {
+        closeLetters.push(value);
+        cannotBeLetters[letterIndex].push(value);
+      } else if (state === 'correct') {
+        closeLetters.push(value);
+        if (correctLetters[letterIndex]) {
+          alert(`It looks like you have two letters marked for the same position: ${value} and ${correctLetters[letterIndex]}`);
+        }
+        correctLetters[letterIndex] = value;
+      } else {
+        debugger;
+      }
+    });
+
+    return {
+      closeLetters,
+      wrongLetters,
+      correctLetters,
+      cannotBeLetters,
+    };
+  }
+
+  function Matcher({
+    closeLetters,
+    wrongLetters,
+    correctLetters,
+    cannotBeLetters,
+  }) {
+    function any(word, letters) {
+      for(let i = 0; i < letters.length; i++) {
+        const letter = letters[i];
+        if (word.includes(letter)) return true;
+      }
+      return false;
+    }
+
+    function all(word, letters) {
+      for(let i = 0; i < letters.length; i++) {
+        const letter = letters[i];
+        if (!word.includes(letter)) return false;
+      }
+      return true;
+    }
+
+    function correctLettersMatch(word, letters) {
+      for(let i = 0; i < letters.length; i++) {
+        const letter = letters[i];
+        if (letter !== undefined && word[i] !== letter) return false;
+      }
+      return true;
+    }
+
+    // confirms that the close letters do not appear in the same positions
+    function cannotBeLettersMatch(word, letters2DArray) { // letters is a 2D array
+      for(let i = 0; i < letters2DArray.length; i++) {
+        const letters = letters2DArray[i];
+        for (let j = 0; j < letters.length; j++) {
+          const letter = letters[j];
+          if (word[i] === letter) return false;
+        }
+      }
+      return true;
+    }
+
+    this.matches = (word) => {
+      if (!correctLettersMatch(word, correctLetters)) return false;
+      if (any(word, wrongLetters)) return false;
+      if (!all(word, closeLetters)) return false;
+      if (!cannotBeLettersMatch(word, cannotBeLetters)) return false;
+      return true;
+    };
   }
 
   function calculate() {
     console.log('calculate');
     // check letter states
-    // make correct letters
-    // make good letters
-    // make bad letters
+    const {
+      closeLetters,
+      wrongLetters,
+      correctLetters,
+      cannotBeLetters,
+    } = getLetters();
     // look for conflicts
     // look for letters used twice
     // find matches
+
+    const potentialMatches = [];
+
+    const matcher = new Matcher({
+      closeLetters,
+      wrongLetters,
+      correctLetters,
+      cannotBeLetters,
+    });
+
+    dictionary.forEach((word) => {
+      const upperCaseWord = word.toUpperCase();
+      // console.log(word, matcher.matches(upperCaseWord));
+      if (matcher.matches(upperCaseWord)) potentialMatches.push(word);
+    });
+
+    console.table({
+      closeLetters: closeLetters.toString(),
+      wrongLetters: wrongLetters.toString(),
+      correctLetters: correctLetters.toString(),
+      cannotBeLetters: cannotBeLetters.toString(),
+    });
+
+    const wordElements = potentialMatches.map(word => `<span>${word}</span>`);
+    answersElement.innerHTML = wordElements.join(' ');
   }
 
   function checkboxUpdated({ srcElement }) {
