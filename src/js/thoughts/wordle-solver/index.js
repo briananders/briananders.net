85798581f29c1c543982ef9e1abed19b60506a34
Brian Anders <banders@google.com>
2022-04-23 22:27:00 -0700

    wordle improve the interface.

--- src/js/thoughts/wordle-solver/index.js
+++ src/js/thoughts/wordle-solver/index.js
@@ -1,17 +1,72 @@
-const { WorkDocs } = require('aws-sdk');
-
 (function init() {
+  const copy = require('copy-to-clipboard');
 
   const dictionary = require('./five-letter-words');
+  const Matcher = require('./matcher');
 
   const boardElement = document.getElementById('board');
   const answersElement = document.getElementById('options');
+  const resultsElement = document.getElementById('results-count');
   const lines = Array.from(boardElement.querySelectorAll('.line'));
   const textInputs = Array.from(boardElement.querySelectorAll('input[type=text'));
   const checkboxes = Array.from(boardElement.querySelectorAll('input[type=radio]'));
   const submitButtons = Array.from(boardElement.querySelectorAll('button'));
 
+  const breakpoint = matchMedia('(max-width: 500px)');
+
+  /////////////////////////// CONSTANTS
+
+  const STATES = {
+    CLOSE: 'close',
+    CORRECT: 'correct',
+    WRONG: 'wrong',
+  };
+
+  const EVENTS = {
+    KEYDOWN: 'keydown',
+    CLICK: 'click',
+    CHANGE: 'change',
+  }
+
+  const KEYS = {
+    BACKSPACE: 'Backspace',
+    ARROW_LEFT: 'ArrowLeft',
+    TAB: 'Tab',
+    ARROW_RIGHT: 'ArrowRight',
+  }
+
+  const EMPTY = '';
+  const SPACE = ' ';
+  const DASH = '-';
+
   const isLetter = (value) => /[a-zA-Z]/.test(value);
+
+  ///////////////// functions
+
+  function checkLinesFull() {
+    const lineFull = new Array(6);
+    let lastFullLine = 0;
+
+    lines.forEach(lineElement => {
+      const inputs = Array.from(lineElement.querySelectorAll('input[type=text]'));
+      const [lineWord, lineNumber] = lineElement.id.split(DASH);
+
+      const isFull = (inputs.filter(input => input.value.length).length === 5);
+      lineFull[Number(lineNumber)] = isFull;
+    });
+
+    for (let i = lineFull.length - 1; i >= 0; i--) {
+      if(lineFull[i]) {
+        lastFullLine = i + 1;
+        break;
+      }
+    }
+
+    if (lastFullLine > 5) lastFullLine = 5;
+
+    const lastFullLineElement = lines.filter(line => Number(line.id.split('-')[1]) === lastFullLine)[0];
+    boardElement.style.height = `${lastFullLineElement.offsetHeight * (lastFullLine + 1)}px`;
+  }
 
   function getLetters() {
     let closeLetters = [];
@@ -23,14 +78,14 @@ const { WorkDocs } = require('aws-sdk');
       const { state, letterNumber } = inputElement.dataset;
       const letterIndex = Number(letterNumber);
       const { value } = inputElement;
-      if (value === '') return;
+      if (value === EMPTY) return;
 
-      if (state === 'wrong') {
+      if (state === STATES.WRONG) {
         wrongLetters.push(value);
-      } else if (state === 'close') {
+      } else if (state === STATES.CLOSE) {
         closeLetters.push(value);
         cannotBeLetters[letterIndex].push(value);
-      } else if (state === 'correct') {
+      } else if (state === STATES.CORRECT) {
         closeLetters.push(value);
         if (correctLetters[letterIndex] && correctLetters[letterIndex] !== value) {
           alert(`It looks like you have two letters marked for the same position: ${value} and ${correctLetters[letterIndex]}`);
@@ -47,68 +102,31 @@ const { WorkDocs } = require('aws-sdk');
     };
   }
 
-  function Matcher({
-    closeLetters,
-    wrongLetters,
-    correctLetters,
-    cannotBeLetters,
-  }) {
-    function any(word, letters) {
-      for(let i = 0; i < letters.length; i++) {
-        const letter = letters[i];
-        if (word.includes(letter)) return true;
-      }
-      return false;
-    }
+  function resetLetter(textInput) {
+    const [letterWord, lineNumber, letterNumber] = textInput.id.split(DASH);
 
-    function all(word, letters) {
-      for(let i = 0; i < letters.length; i++) {
-        const letter = letters[i];
-        if (!word.includes(letter)) return false;
-      }
-      return true;
-    }
+    const closeCheckbox = document.getElementById(`${STATES.CLOSE}-${lineNumber}-${letterNumber}`);
+    const correctCheckbox = document.getElementById(`${STATES.CORRECT}-${lineNumber}-${letterNumber}`);
+    const wrongCheckbox = document.getElementById(`${STATES.WRONG}-${lineNumber}-${letterNumber}`);
 
-    function correctLettersMatch(word, letters) {
-      for(let i = 0; i < letters.length; i++) {
-        const letter = letters[i];
-        if (letter !== undefined && word[i] !== letter) return false;
-      }
-      return true;
-    }
+    textInput.value = EMPTY;
+    textInput.dataset.state = STATES.WRONG;
 
-    // confirms that the close letters do not appear in the same positions
-    function cannotBeLettersMatch(word, letters2DArray) { // letters is a 2D array
-      for(let i = 0; i < letters2DArray.length; i++) {
-        const letters = letters2DArray[i];
-        for (let j = 0; j < letters.length; j++) {
-          const letter = letters[j];
-          if (word[i] === letter) return false;
-        }
-      }
-      return true;
-    }
-
-    this.matches = (word) => {
-      if (!correctLettersMatch(word, correctLetters)) return false;
-      if (any(word, wrongLetters)) return false;
-      if (!all(word, closeLetters)) return false;
-      if (!cannotBeLettersMatch(word, cannotBeLetters)) return false;
-      return true;
-    };
+    wrongCheckbox.checked = true;
+    closeCheckbox.checked = false;
+    correctCheckbox.checked = false;
   }
 
   function calculate() {
-    // check letter states
+
+    checkLinesFull();
+
     const {
       closeLetters,
       wrongLetters,
       correctLetters,
       cannotBeLetters,
     } = getLetters();
-    // look for conflicts
-    // look for letters used twice
-    // find matches
 
     const potentialMatches = [];
 
@@ -132,18 +150,20 @@ const { WorkDocs } = require('aws-sdk');
     });
 
     const wordElements = potentialMatches.map(word => `<span>${word}</span>`);
-    answersElement.innerHTML = wordElements.join(' ');
+    answersElement.innerHTML = wordElements.join(SPACE);
+
+    resultsElement.innerText = wordElements.length;
   }
 
   function checkboxUpdated({ srcElement }) {
     if (!srcElement.checked) return;
-    const [state, lineNumber, letterNumber] = srcElement.id.split('-');
+    const [state, lineNumber, letterNumber] = srcElement.id.split(DASH);
     const letterInput = document.getElementById(`letter-${lineNumber}-${letterNumber}`);
     letterInput.dataset.state = state;
   }
 
   function previousInput(srcElement) {
-    const [letterWord, lineNumber, letterNumber] = srcElement.id.split('-');
+    const [letterWord, lineNumber, letterNumber] = srcElement.id.split(DASH);
 
     const nextInput = document.getElementById(`${letterWord}-${lineNumber}-${Number(letterNumber) - 1}`);
     if (nextInput) {
@@ -152,7 +172,7 @@ const { WorkDocs } = require('aws-sdk');
   }
 
   function nextInput(srcElement) {
-    const [letterWord, lineNumber, letterNumber] = srcElement.id.split('-');
+    const [letterWord, lineNumber, letterNumber] = srcElement.id.split(DASH);
 
     const nextInput = document.getElementById(`${letterWord}-${lineNumber}-${Number(letterNumber) + 1}`);
     if (nextInput) {
@@ -165,14 +185,14 @@ const { WorkDocs } = require('aws-sdk');
     const char = key.toUpperCase();
 
     switch (key) {
-      case 'Backspace':
-        srcElement.value = '';
-      case 'ArrowLeft':
+      case KEYS.BACKSPACE:
+        resetLetter(srcElement);
+      case KEYS.ARROW_LEFT:
         previousInput(srcElement);
         evt.preventDefault();
         return;
-      case 'Tab':
-      case 'ArrowRight':
+      case KEYS.TAB:
+      case KEYS.ARROW_RIGHT:
         nextInput(srcElement);
         evt.preventDefault();
         return;
@@ -180,26 +200,39 @@ const { WorkDocs } = require('aws-sdk');
     }
 
     if (isLetter(char) && char.length === 1) {
+      resetLetter(srcElement);
       srcElement.value = char;
       nextInput(srcElement);
       evt.preventDefault();
     } else {
-      srcElement.value = '';
+      resetLetter(srcElement);
     }
+
+    checkLinesFull();
   }
 
   function initEventListeners() {
     textInputs.forEach(input => {
-      input.addEventListener('keydown', inputKeydown);
+      input.addEventListener(EVENTS.KEYDOWN, inputKeydown);
     });
     submitButtons.forEach(button => {
-      button.addEventListener('click', calculate);
+      button.addEventListener(EVENTS.CLICK, calculate);
     });
     checkboxes.forEach(box => {
-      box.addEventListener('change', checkboxUpdated)
-    })
+      box.addEventListener(EVENTS.CHANGE, checkboxUpdated)
+    });
+    answersElement.addEventListener(EVENTS.CLICK, ({ target }) => {
+      if (target.tagName !== 'SPAN') return;
+      copy(target.innerText);
+      target.classList.add('animate');
+      setTimeout(() => {
+        target.classList.remove('animate');
+      }, 1500);
+    });
+    breakpoint.addEventListener('change', checkLinesFull);
   }
 
   initEventListeners();
+  checkLinesFull();
 
 }());
