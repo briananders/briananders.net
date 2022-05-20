941776237bef3f52b853712a07df5a4bd1e5bfa2
Brian Anders <banders@google.com>
2022-05-20 00:18:57 -0700

    Ang life simulator motion is working.

--- src/js/thoughts/ant-life-simulator.js
+++ src/js/thoughts/ant-life-simulator.js
@@ -1,5 +1,12 @@
 const { log } = require('../_modules/log');
 
+const ANT = 'ant';
+const ANT_EATER = 'anteater'
+
+const STARTING_ANT_COUNT = 12;
+const STARTING_ANT_EATER_COUNT = 3;
+
+
 function Cell(element) {
   this.x = Number(element.dataset.x);
   this.y = Number(element.dataset.y);
@@ -10,9 +17,12 @@ function Cell(element) {
   }
 
   this.render = () => {
-    log(`render cell ${this.x},${this.y}: ${this.value}`);
+    // log(`render cell ${this.x},${this.y}: ${this.value}`);
     element.dataset.value = (this.value) ? this.value.toString() : '';
   }
+
+  this.isAnt = () => this.value === ANT;
+  this.isAntEater = () => this.value === ANT_EATER;
 }
 
 function Board(element) {
@@ -20,12 +30,6 @@ function Board(element) {
   /*
     Private variables
   */
-
-  const startingAntCount = 6;
-  const startingAntEaterCount = 2;
-
-  const ANT = 'ant';
-  const ANT_EATER = 'anteater'
 
   const size = Number(element.dataset.size);
 
@@ -53,7 +57,7 @@ function Board(element) {
   };
 
   const initializeAnts = () => {
-    let antsToPlace = startingAntCount;
+    let antsToPlace = STARTING_ANT_COUNT;
 
     while (antsToPlace > 0) {
       const randomX = Math.floor(Math.random() * width);
@@ -67,7 +71,7 @@ function Board(element) {
   };
 
   const initializeAntEaters = () => {
-    let antEatersToPlace = startingAntEaterCount;
+    let antEatersToPlace = STARTING_ANT_EATER_COUNT;
 
     while (antEatersToPlace > 0) {
       const randomX = Math.floor(Math.random() * width);
@@ -81,6 +85,8 @@ function Board(element) {
   };
 
   const render = () => {
+    log(`${Date.now()}: render board`);
+
     board.forEach((row) => {
       row.forEach((cell) => {
         cell.render();
@@ -88,12 +94,116 @@ function Board(element) {
     });
   };
 
+  const moveFromTo = ([x, y], [newX, newY], type) => {
+    const oldCell = getCell(x, y);
+    const newCell = getCell(newX, newY);
+
+    oldCell.value = undefined;
+    newCell.value = type;
+
+    oldCell.render();
+    newCell.render();
+
+    // log(`[${x}, ${y}], [${newX}, ${newY}]`);
+  };
+
+  const moveLeft = (x, y, type) => {
+    if (x - 1 >= 0 && getCell(x - 1, y).isEmpty()) {
+      // log('moveLeft');
+      moveFromTo([x, y], [x - 1, y], type);
+    }
+  };
+  const moveRight = (x, y, type) => {
+    if (x + 1 < width - 1 && getCell(x + 1, y).isEmpty()) {
+      // log('moveRight');
+      moveFromTo([x, y], [x + 1, y], type);
+    }
+  };
+  const moveDown = (x, y, type) => {
+    if (y - 1 >= 0 && getCell(x, y - 1).isEmpty()) {
+      // log('moveDown');
+      moveFromTo([x, y], [x, y - 1], type);
+    }
+  };
+  const moveUp = (x, y, type) => {
+    if (y + 1 < height - 1 && getCell(x, y + 1).isEmpty()) {
+      // log('moveUp');
+      moveFromTo([x, y], [x, y + 1], type);
+    }
+  };
+
+  const getAnts = () => {
+    const ants = [];
+
+    board.forEach((row) => {
+      row.forEach((cell) => {
+        if (cell.isAnt()) ants.push(cell);
+      });
+    });
+
+    return ants;
+  }
+
+  const getAntEaters = () => {
+    const antEaters = [];
+
+    board.forEach((row) => {
+      row.forEach((cell) => {
+        if (cell.isAntEater()) antEaters.push(cell);
+      });
+    });
+
+    return antEaters;
+  }
+
   const moveAnts = () => {
-    log('moveAnts');
+    // log('moveAnts');
+
+    const ants = getAnts();
+    ants.forEach(ant => {
+      const { x, y } = ant;
+
+      switch(Math.floor(Math.random() * 4)) {
+        case 0: // left
+          moveLeft(x, y, ANT);
+          break;
+        case 1: // right
+          moveRight(x, y, ANT);
+          break;
+        case 2: // up
+          moveUp(x, y, ANT);
+          break;
+        default: // down
+          moveDown(x, y, ANT);
+      }
+    });
+
+    // check for procreation
   };
 
   const moveAntEaters = () => {
-    log('moveAntEaters');
+    // log('moveAntEaters');
+
+    const antEaters = getAntEaters();
+    antEaters.forEach(antEater => {
+      const { x, y } = antEater;
+
+      switch(Math.floor(Math.random() * 4)) {
+        case 0: // left
+          moveLeft(x, y, ANT_EATER);
+          break;
+        case 1: // right
+          moveRight(x, y, ANT_EATER);
+          break;
+        case 2: // up
+          moveUp(x, y, ANT_EATER);
+          break;
+        default: // down
+          moveDown(x, y, ANT_EATER);
+      }
+    });
+
+    // check for procreation
   };
 
   const getCell = (x, y) => board[x][y];
@@ -104,15 +214,18 @@ function Board(element) {
     getCell(x, y).value = value;
   };
 
-  /*
-    Public variables
-  */
-
-  this.move = () => {
+  const move = () => {
     moveAnts();
     moveAntEaters();
-    render();
+    // render();
   };
+
+  /*
+    Public Function
+  */
+
+  this.move = move;
+  this.render = render;
 
   /*
     Function Calls
@@ -127,7 +240,7 @@ function Board(element) {
 (function init() {
   const boardElement = document.getElementById('board');
   const board = new Board(boardElement);
-  board.move();
 
-  // setInterval(board.move, 1000);
+  board.render();
+  setInterval(board.move, 1000);
 }());
