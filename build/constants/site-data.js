be18adf2aad4d1cb7fbf362148804421c38d2c93
Brian Anders <banders@google.com>
2023-05-24 12:42:45 -0700

    new mastodon account location. updated the config file to reflect the change.

--- build/constants/site-data.js
+++ build/constants/site-data.js
@@ -18,7 +18,7 @@ module.exports = (dir) => {
     twitter,
     github: 'https://github.com/briananders',
     linkedin: 'https://www.linkedin.com/in/andersbrian/',
-    mastodon: 'https://musician.social/@banders',
+    mastodon: 'https://mastodon.social/@banders',
     coyoteDrums: {
       youtube: 'https://www.youtube.com/@coyote_drums',
       path: '/coyote-drums',
