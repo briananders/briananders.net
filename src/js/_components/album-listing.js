fd799805c5bf91ba71c0bcad869859043abca2cb
Brian Anders <banders@google.com>
2024-06-23 22:17:49 -0500

    WIP: design system page

--- src/js/_components/album-listing.js
+++ src/js/_components/album-listing.js
@@ -11,7 +11,7 @@ const albumTemplate = `
       transition: box-shadow 300ms ease, background-color 300ms ease;
     }
     a:hover {
-      box-shadow: -3px 0px 0px var(--palette--primary-grey), -9px 0px 0px var(--palette--primary-color-light);
+      box-shadow: 0px 0px 0px 6px var(--palette--primary-grey), 0px 0px 0px 8px var(--palette--primary-color-light);
     }
     a:focus {
       background-color: var(--palette--secondary-grey);
